c6e06ea98350e989532d8d8245dffc1c86fa5498
Steven Tan <chienleng@gmail.com>
2017-03-26 19:42:42 +1100

    refactor

--- js/client.js
+++ js/client.js
@@ -1,3 +1,49 @@
+/* client functions */
+'use strict';
+
+window.chartColors = {
+	red: 'rgba(255, 99, 132, 0.5)',
+	orange: 'rgba(255, 159, 64, 0.5)',
+	yellow: 'rgba(255, 205, 86, 0.5)',
+	green: 'rgba(75, 192, 192, 0.5)',
+	blue: 'rgba(54, 162, 235, 0.5)',
+	purple: 'rgba(153, 102, 255, 0.5)',
+	grey: 'rgba(201, 203, 207, 0.2)'
+};
+
+window.greyColors = {
+	grey1: 'rgb(227, 227, 227)',
+	grey2: 'rgb(213, 213, 213)',
+	grey3: 'rgb(198, 198, 198)',
+	grey4: 'rgb(181, 181, 181)',
+	grey5: 'rgb(165, 165, 165)',
+	grey6: 'rgb(143, 143, 143)',
+	grey7: 'rgb(125, 125, 125)',
+	grey8: 'rgb(104, 104, 104)'
+};
+
+window.randomScalingFactor = function() {
+	// return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 1600);
+	return (Math.random() > 0.5 ? 1.0 : 1.0) * Math.round(Math.random() * 1600);
+};
+
+// var csv is the CSV file with headers
+function csvJSON(csv) {
+  var lines = csv.split("\n");
+  var result = [];
+  var headers=lines[0].split(",");
+  for(var i=1;i<lines.length;i++){
+      var obj = {};
+      var currentline=lines[i].split(",");
+      for(var j=0;j<headers.length;j++){
+        obj[j] = currentline[j];
+      }
+      result.push(obj);
+  }
+  return result; //JavaScript object
+  // return JSON.stringify(result); //JSON
+}
+
 function showNasties() {
   var nasties = [
     '<h1>534 kg CO<sub>2</sub></h1><h4>per second</h4>',
@@ -65,7 +111,7 @@ function setupChart() {
 
 function getDispatchData() {
   nanoajax.ajax({
-    url:'/dispatch.csv'},
+    url:'/data/dispatch.csv'},
     function (code, responseText) {
       var j = csvJSON(responseText);
       j.pop();
